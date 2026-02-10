5c82c1d332119d83763eddf0d3dfb221e034e0b2
SyncIt Build <rav@syncit.ac>
2026-02-10 10:52:19 +0000

    File changes

--- functions/transcribeLyrics.ts
+++ functions/transcribeLyrics.ts
@@ -1,4 +1,4 @@
-import { createClientFromRequest } from 'npm:@base44/sdk@0.7.1';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.6';
 
 export async function transcribeLyrics({ audioUrl }) {
     // This is a wrapper function that can be called from the frontend
